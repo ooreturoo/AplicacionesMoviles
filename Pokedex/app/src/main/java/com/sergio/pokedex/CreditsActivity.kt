d8ac7bc6292ff4a4fbc83d84472a21bbeeeeb8dd
Sergio <sergiomm50@gmail.com>
2022-10-19 19:47:58 +0200

    A falta de función del botón contacto

--- Pokedex/app/src/main/java/com/sergio/pokedex/CreditsActivity.kt
+++ Pokedex/app/src/main/java/com/sergio/pokedex/CreditsActivity.kt
@@ -1,11 +1,33 @@
 package com.sergio.pokedex
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class CreditsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_credits)
+        takeAndShowUser()
+
+
     }
+
+    private fun takeAndShowUser(){
+
+        val user = intent.extras!!.get("User")
+        val tvGreeting = findViewById<TextView>(R.id.tvGreetingUser)
+        tvGreeting.text = getString(R.string.tv_user_greeting, user)
+
+    }
+
+    private fun sendMail(){
+
+
+
+    }
+
+
+
 }
