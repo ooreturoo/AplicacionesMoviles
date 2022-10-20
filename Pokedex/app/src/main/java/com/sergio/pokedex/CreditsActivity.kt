e260186cb4bdc225d3fc82707c985a786c0382a8
Sergio <sergiomm50@gmail.com>
2022-10-20 12:55:39 +0200

    splash a medias

--- Pokedex/app/src/main/java/com/sergio/pokedex/CreditsActivity.kt
+++ Pokedex/app/src/main/java/com/sergio/pokedex/CreditsActivity.kt
@@ -1,15 +1,22 @@
 package com.sergio.pokedex
 
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.TextView
+import android.widget.Toast
 
 class CreditsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_credits)
         takeAndShowUser()
+        findViewById<TextView>(R.id.btnContact).setOnClickListener{
+            sendMail()
+        }
 
 
     }
@@ -24,6 +31,25 @@ class CreditsActivity : AppCompatActivity() {
 
     private fun sendMail(){
 
+
+        val intent = Intent(Intent.ACTION_SEND).apply {
+
+            type="text/plain"
+            putExtra(Intent.EXTRA_EMAIL, arrayOf("smatmor813@g.educaand.es"))
+            putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mail_subject,getString(R.string.app_name)))
+            putExtra(Intent.EXTRA_TEXT, getString(R.string.mail_content,getString(R.string.app_name)))
+
+        }
+
+        if (intent.resolveActivity(packageManager) != null) {
+
+            startActivity(intent)
+
+        }else{
+
+            Toast.makeText(this,"No se puede manejar el intent",Toast.LENGTH_SHORT).show()
+
+        }
 
 
     }
---
+++ Pokedex/app/src/main/java/com/sergio/pokedex/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.sergio.pokedex
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this,MainActivity::class.java))
+    }
+}
